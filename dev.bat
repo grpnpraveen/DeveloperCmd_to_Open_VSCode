8daf71e9e4227dba41fde9432942b89133af83cf
Gali Ravi Praveen <grpnpraveen@gmail.com>
2021-12-23 13:07:08 +0530

    Updated to more optimised one .

--- dev.bat
+++ dev.bat
@@ -1,4 +1,1 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-code .
-sleep 3
-exit
+call %comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat" && code . && exit"
