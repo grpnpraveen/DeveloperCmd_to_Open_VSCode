b8ad6ba7506296e70e209970699589926b595fc2
Gali Ravi Praveen <grpnpraveen@gmail.com>
2021-12-24 07:10:17 +0530

    Update devgitbash.bat

--- devgitbash.bat
+++ devgitbash.bat
@@ -1,1 +1,1 @@
-call %comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat" &&cd C:\Program Files\Git && git-bash.exe && exit"
+start %comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat" &&cd C:\Program Files\Git && start /b git-bash.exe && exit"
